3003328bced25217e21b3f9479a57f806485b585
Bartosz Cieśla <bartosz.ciesla@gmail.com>
2026-02-05 18:18:14 +0100

    test(cli): add snapshot tests for command help output
    Add comprehensive snapshot tests for CLI help messages using insta with
    filtering support to handle non-deterministic build metadata. Tests cover
    both short (-h) and long (--help) flag variations, as well as the default
    command overview output.

---
+++ tests/int/command.rs
@@ -0,0 +1,26 @@
+use crate::common::crawk;
+use insta::with_settings;
+use insta_cmd::assert_cmd_snapshot;
+use test_case::test_case;
+
+#[test]
+fn should_command_overview_match() {
+    assert_cmd_snapshot!(crawk());
+}
+
+#[test_case("-h"; "short help")]
+#[test_case("--help"; "long help")]
+fn should_command_short_help_match(help_flag: &str) {
+    let flag = help_flag.trim_start_matches('-');
+    let snapshot_name = format!("flag_{flag}");
+
+    // Filter out non-deterministic part with build timestamp
+    with_settings!({
+    filters => vec![
+        (r"Built on \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z for [\w-]+ \([\d.]+\) by \w+",
+         "Built on [TIMESTAMP] for [TARGET] ([RUSTC]) by [USER]"),
+    ],
+    }, {
+        assert_cmd_snapshot!(snapshot_name, crawk().arg(help_flag));
+    });
+}
--- tests/int/mod.rs
+++ tests/int/mod.rs
@@ -1,1 +1,2 @@
+mod command;
 mod use_command;
